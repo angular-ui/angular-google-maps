373c6ade06215be6946151e73adae4ee2352d9ab
nlaplante <nicolas.laplante@gmail.com>
2012-11-26 18:17:25 -0500

    Remove markers from map when removed from scope

--- src/angular-google-maps.js
+++ src/angular-google-maps.js
@@ -195,7 +195,7 @@
 				
 				// Return marker instance
 				return marker;
-			};
+			};			
 			
 			this.findMarker = function (lat, lng) {
 				for (var i = 0; i < _markers.length; i++) {
@@ -208,6 +208,18 @@
 				
 				return null;
 			};	
+			
+			this.findMarkerIndex = function (lat, lng) {
+				for (var i = 0; i < _markers.length; i++) {
+					var pos = _markers[i].getPosition();
+					
+					if (pos.lat() == lat && pos.lng() == lng) {
+						return i;
+					}
+				}
+				
+				return -1;
+			};
 			
 			this.addInfoWindow = function (lat, lng, html) {
 				var win = new google.maps.InfoWindow({
@@ -223,6 +235,29 @@
 			this.hasMarker = function (lat, lng) {
 				return that.findMarker(lat, lng) !== null;
 			};	
+			
+			this.getMarkerInstances = function () {
+				return _markers;
+			};
+			
+			this.removeMarkers = function (markerInstances) {
+				
+				var s = this;
+				
+				angular.forEach(markerInstances, function (v, i) {
+					var pos = v.getPosition(),
+						lat = pos.lat(),
+						lng = pos.lng(),
+						index = s.findMarkerIndex(lat, lng);
+					
+					// Remove from local arrays
+					_markers.splice(index, 1);
+					s.markers.splice(index, 1);
+					
+					// Remove from map
+					v.setMap(null);
+				});
+			};
 		}
 		
 		// Done
@@ -258,8 +293,11 @@
 			},
 			controller: function ($scope, $element) {
 				
-				this.addInfoWindow = function (lat, lng, content) {
-					$scope.map.addInfoWindow(lat, lng, content);
+				var self = this,
+					_m = $scope.map;
+				
+				self.addInfoWindow = function (lat, lng, content) {
+					_m.addInfoWindow(lat, lng, content);
 				};
 			},			
 			link: function (scope, element, attrs, ctrl) {
@@ -278,7 +316,7 @@
 				angular.element(element).addClass("angular-google-map");
 				
 				// Create our model
-				scope.map = new MapModel({
+				var _m = new MapModel({
 					container: element[0],
 						
 					center: new google.maps.LatLng(scope.center.lat, 
@@ -287,56 +325,56 @@
 					draggable: attrs.draggable == "true",
 					
 					zoom: scope.zoom
-				});
+				});				
 			
-				scope.map.on("drag", function () {
+				_m.on("drag", function () {
 					
-					var c = scope.map.center;
+					var c = _m.center;
 				
 					$timeout(function () {
 						
 						scope.$apply(function (s) {
-							s.center.lat = c.lat();
-							s.center.lng = c.lng();
+							scope.center.lat = c.lat();
+							scope.center.lng = c.lng();
 						});
 					});
 				});
 			
-				scope.map.on("zoom_changed", function () {
+				_m.on("zoom_changed", function () {
 					
-					if (scope.zoom != scope.map.zoom) {
+					if (scope.zoom != _m.zoom) {
 						
 						$timeout(function () {
 							
 							scope.$apply(function (s) {
-								s.zoom = scope.map.zoom;
+								scope.zoom = _m.zoom;
 							});
 						});
 					}
 				});
 			
-				scope.map.on("center_changed", function () {
-					var c = scope.map.center;
+				_m.on("center_changed", function () {
+					var c = _m.center;
 				
 					$timeout(function () {
 						
 						scope.$apply(function (s) {
 							
-							if (!s.map.dragging) {
-								s.center.lat = c.lat();
-								s.center.lng = c.lng();
+							if (!_m.dragging) {
+								scope.center.lat = c.lat();
+								scope.center.lng = c.lng();
 							}
 						});
 					});
 				});
 				
-				if (attrs.markClick) {
+				if (attrs.markClick == "true") {
 					(function () {
 						var cm = null;
 						
-						scope.map.on("click", function (e) {													
+						_m.on("click", function (e) {													
 							if (cm == null) {
-								cm = scope.map.addMarker(e.latLng.lat(), 
+								cm = _m.addMarker(e.latLng.lat(), 
 										e.latLng.lng());
 								
 								scope.markers.push({
@@ -351,8 +389,8 @@
 									cm_lng = cm_position.lng(),								
 									filtered_markers = $filter("filter")(
 											scope.markers, 
-											function (m) {
-												
+											
+											function (m) {												
 												return m.latitude == cm_lat &&
 													m.longitude == cm_lng;								
 											}
@@ -377,27 +415,69 @@
 					}());
 				}
 				
+				// Done, draw the map
+				_m.draw();
+				
+				// Put the map into the scope
+				scope.map = _m;
+				
 				// Check if we need to refresh the map
 				scope.$watch("refresh()", function (newValue, oldValue) {
 					if (newValue) {
-						scope.map.draw();
+						_m.draw();
 					}
 				});	
 				
 				// Markers
 				scope.$watch("markers", function (newValue, oldValue) {
 					
-					angular.forEach(newValue, function (v, i) {
-						if (!scope.map.hasMarker(v.latitude, v.longitude)) {
-							scope.map.addMarker(v.latitude, v.longitude);
+					$timeout(function () {
+						
+						angular.forEach(newValue, function (v, i) {
+							if (!_m.hasMarker(v.latitude, v.longitude)) {
+								_m.addMarker(v.latitude, v.longitude);
+							}
+						});
+						
+						// Clear orphaned markers
+						var orphaned = [];
+						
+						angular.forEach(_m.getMarkerInstances(), function (v, i) {
+							// Check our scope if a marker with equal latitude and longitude. 
+							// If not found, then that marker has been removed form the scope.
+							
+							var pos = v.getPosition(),
+								lat = pos.lat(),
+								lng = pos.lng(),
+								found = false;
+							
+							// Test against each marker in the scope
+							for (var si = 0; si < scope.markers.length; si++) {
+								
+								var sm = scope.markers[si];
+								
+								if (sm.latitude == lat && sm.longitude == lng) {
+									// Map marker is present in scope too, don't remove
+									found = true;
+								}
+							}
+							
+							// Marker in map has not been found in scope. Remove.
+							if (!found) {
+								orphaned.push(v);
+							}
+						});
+						
+						_m.removeMarkers(orphaned);
+						
+						
+						// Fit map when there are more than one marker. 
+						// This will change the map center coordinates
+						if (newValue.length > 1) {
+							_m.fit();
 						}
 					});
 					
-					// Fit map when there are more than one marker. 
-					// This will change the map center coordinates
-					if (newValue.length > 1) {
-						scope.map.fit();
-					}
 				}, true);
 				
 				
@@ -407,10 +487,10 @@
 						return;
 					}
 					
-					if (!scope.map.dragging) {
-						scope.map.center = new google.maps.LatLng(newValue.lat, 
+					if (!_m.dragging) {
+						_m.center = new google.maps.LatLng(newValue.lat, 
 								newValue.lng);					
-						scope.map.draw();
+						_m.draw();
 					}
 				}, true);
 				
@@ -419,21 +499,19 @@
 						return;
 					}
 					
-					scope.map.zoom = newValue;
-					scope.map.draw();
+					_m.zoom = newValue;
+					_m.draw();
 				});
 				
-				// Done, draw the map
-				scope.map.draw();
+				
 			},
 		};
 	});
 	
 	/**
-	 * Rectangle directive
+	 * Information window directive (WIP, not ready for production)
 	 */
-	googleMapsModule.directive("infoWindow", function ($log, $timeout, 
-			$compile, $controller) {
+	googleMapsModule.directive("infoWindow", function ($log, $timeout, $compile) {
 		
 		return {
 			restrict: 'E',
