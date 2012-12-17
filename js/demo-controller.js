5d3c01a31f5be5e971892e60be947ed2d168303e
nlaplante <nicolas.laplante@gmail.com>
2012-12-17 07:05:05 -0500

    Move DemoController inside anonymous function

--- js/demo-controller.js
+++ js/demo-controller.js
@@ -1,80 +1,3 @@
-function DemoController ($scope, $location)
-{
-	
-	$scope.activeTab = $location.path() == $scope.basePath ? "usage" : $location.path().substr(1);
-	
-	$scope.$watch("activeTab", function (newValue, oldValue) {
-		if (newValue === oldValue) {
-			return;
-		}
-		
-		$location.path($scope.basePath +  $scope.activeTab);
-	});
-	
-	$scope.$watch(function () {
-		return $location.path();
-	}, function (newValue, oldValue) {
-		if (newValue === oldValue) {
-			return;
-		}
-		
-		$scope.activeTab = newValue.substr($scope.basePath.length);
-	});
-	
-	$scope.getNavClass = function (item) {
-		return item == $scope.activeTab ? "active" : "";
-	};
-	
-	// Default New York
-	$scope.center = {
-		lat: 40.69847032728747,
-		lng: -73.9514422416687
-	};
-	
-	$scope.geolocationAvailable = navigator.geolocation ? true : false;
-	
-	$scope.latitude = null;
-	$scope.longitude = null;
-	
-	$scope.zoom = 4;
-	
-	$scope.markers = [];
-	
-	$scope.markerLat = null;
-	$scope.markerLng = null;
-	
-	$scope.addMarker = function () {
-		$scope.markers.push({
-			latitude: parseFloat($scope.markerLat),
-			longitude: parseFloat($scope.markerLng)
-		});
-		
-		$scope.markerLat = null;
-		$scope.markerLng = null;
-	};
-	
-	$scope.findMe = function () {
-		
-		if ($scope.geolocationAvailable) {
-			
-			navigator.geolocation.getCurrentPosition(function (position) {
-				
-				$scope.center = {
-					lat: position.coords.latitude,
-					lng: position.coords.longitude
-				};
-				
-				$scope.$apply();
-			}, function () {
-				
-			});
-				
-		}	
-	};
-	
-	
-}
-
 (function () {
 	
 	var module = angular.module("google-maps-demo", ["google-maps", "bootstrap"]);
@@ -100,6 +23,79 @@ function DemoController ($scope, $location)
 					}
 				};
 			}
+		};
+	});
+	
+	module.controller("DemoController", function ($scope, $location) {
+		$scope.activeTab = $location.path() == $scope.basePath ? "usage" : $location.path().substr(1);
+		
+		$scope.$watch("activeTab", function (newValue, oldValue) {
+			if (newValue === oldValue) {
+				return;
+			}
+			
+			$location.path($scope.basePath +  $scope.activeTab);
+		});
+		
+		$scope.$watch(function () {
+			return $location.path();
+		}, function (newValue, oldValue) {
+			if (newValue === oldValue) {
+				return;
+			}
+			
+			$scope.activeTab = newValue.substr($scope.basePath.length);
+		});
+		
+		$scope.getNavClass = function (item) {
+			return item == $scope.activeTab ? "active" : "";
+		};
+		
+		// Default New York
+		$scope.center = {
+			lat: 40.69847032728747,
+			lng: -73.9514422416687
+		};
+		
+		$scope.geolocationAvailable = navigator.geolocation ? true : false;
+		
+		$scope.latitude = null;
+		$scope.longitude = null;
+		
+		$scope.zoom = 4;
+		
+		$scope.markers = [];
+		
+		$scope.markerLat = null;
+		$scope.markerLng = null;
+		
+		$scope.addMarker = function () {
+			$scope.markers.push({
+				latitude: parseFloat($scope.markerLat),
+				longitude: parseFloat($scope.markerLng)
+			});
+			
+			$scope.markerLat = null;
+			$scope.markerLng = null;
+		};
+		
+		$scope.findMe = function () {
+			
+			if ($scope.geolocationAvailable) {
+				
+				navigator.geolocation.getCurrentPosition(function (position) {
+					
+					$scope.center = {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude
+					};
+					
+					$scope.$apply();
+				}, function () {
+					
+				});
+					
+			}	
 		};
 	});
 }());
