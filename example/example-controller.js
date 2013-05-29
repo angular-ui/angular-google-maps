417f9eb2c24b1e8c172150c10399e98fc86259e8
nlaplante <nicolas.laplante@gmail.com>
2013-05-29 15:26:10 -0400

    Modify example to reflect changes in API

--- example/example-controller.js
+++ example/example-controller.js
@@ -9,43 +9,38 @@ function ExampleController ($scope, $timeout, $log) {
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
 	
-	angular.extend($scope, {
-	  
-	    position: {
-	      coords: {
+	angular.extend($scope, {	  
+	    map: {
+	      center: {
 	        latitude: 45,
 	        longitude: -73
-	      }
-	    },
-		
-		/** the initial center of the map */
-		centerProperty: {
-			latitude: 45,
-			longitude: -73
-		},
-		
-		/** the initial zoom level of the map */
-		zoomProperty: 2,
-		
-		/** list of markers to put in the map */
-		markersProperty: [ {
-				latitude: 45,
-				longitude: -74
-			}, {
-			    latitude: 15,
-			    longitude: 30
-			}],
-		
-		// These 2 properties will be set when clicking on the map
-		clickedLatitudeProperty: null,	
-		clickedLongitudeProperty: null,
-		
-		eventsProperty: {
-		  click: function (mapModel, eventName, originalEventArgs) {	
-		    // 'this' is the directive's scope
-		    $log.log("user defined event on map directive with scope", this);
-		    $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
-		  }
-		}
+	      },
+	      zoom: 3,
+	      markers: [ {
+            latitude: 45,
+            longitude: -74
+          }, {
+              latitude: 15,
+              longitude: 30
+          }],
+          clickedMarker: {
+            latitude: null,
+            longitude: null
+          },
+          events: {
+            click: function (mapModel, eventName, originalEventArgs) {    
+
+              // 'this' is the directive's scope
+              $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+              
+              var e = originalEventArgs[0];
+              
+              $scope.map.clickedMarker.latitude = e.latLng.lat();
+              $scope.map.clickedMarker.longitude = e.latLng.lng();
+              
+              $scope.$apply();
+            }
+          }
+	    } 
 	});
 }
