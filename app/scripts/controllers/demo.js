ea909d295d443a012b5d8a770caf24e82c391cfe
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-08 15:26:35 -0500

    Start demo page

--- app/scripts/controllers/demo.js
+++ app/scripts/controllers/demo.js
@@ -1,10 +1,35 @@
 'use strict';
 
-angular.module('angularGoogleMapsApp')
-  .controller('DemoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('angularGoogleMapsApp').controller('DemoCtrl', function ($scope) {
+	
+	$scope.tab = 'status';
+	
+	$scope.map = {
+		center: {
+			latitude: 40.47,	// NYC
+			longitude: -73.85	// NYC
+		},
+		zoom: 8,
+		markers: [{
+				id: 0,
+				latitude: 41,
+				longitude: -75,
+				title: 'Marker 1'
+			}, {
+				id: 1,
+				latitude: 38,
+				longitude: -67,
+				title: 'Marker 2'				
+			}]
+	};
+	
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			$scope.map.center = {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			};
+		});
+	}
+	
+});
