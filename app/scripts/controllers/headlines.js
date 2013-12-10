c222a09bddd999b2b80acd1a5231a1aef39aa60b
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-10 17:32:20 -0500

    Fetch headlines on a regular basis

--- app/scripts/controllers/headlines.js
+++ app/scripts/controllers/headlines.js
@@ -1,19 +1,43 @@
 'use strict';
 
 angular.module('angularGoogleMapsApp')
-  .controller('HeadlinesCtrl', function ($scope, $http, $log, $timeout) {
-
+  .controller('HeadlinesCtrl', function ($scope, $http, $log, $interval, headlinesFetchInterval) {
+  
+  	var showAtMost = 3, loadMoreCount = 10;
+  
 	var fetchHeadlines = function () {
+	
 		$http({
 			method: 'GET',
 			url: '/headlines.json'
 		}).then(function (res) {
 			$scope.headlines = res.data.items;
-			$timeout(fetchHeadlines, 1000 * 300); // check each 5 minutes
+			$scope.count = res.data.items.length;
 		}, function (res) {
 			$log.error('could not fetch headlines', res.status);
 		});
 	};
-
-	$timeout(fetchHeadlines); // fetch headlines immediately
+	
+	$log.info('will fetch headlines every ' + (headlinesFetchInterval / 1000 / 60) + ' minute(s)');
+	
+	var promise = $interval(fetchHeadlines, headlinesFetchInterval);
+	
+	promise.then(function () {
+		// noop
+	}, function (e) {
+		$log.error('an error has occured in interval', e);
+	}, function (headlines) {
+		$log.info('fetched headlines');
+	});
+	
+	$scope.displayed = function () {
+		return _.first($scope.headlines, showAtMost);
+	};
+	
+	$scope.loadMore = function () {
+		showAtMost += loadMoreCount;
+	};
+	
+	// 1st execution
+	fetchHeadlines.apply(this, []);
   });
