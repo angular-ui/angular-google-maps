63ee018dbe1817c01e2b387f0013d0c1eb9d72f9
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-10 17:31:43 -0500

    Heavy footer work

---
+++ app/scripts/controllers/footer.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('angularGoogleMapsApp').controller('FooterCtrl', function ($scope, $log, $q, $github) {
+
+    // GitHub api calls
+  	$q.all([$github.getCommits(), /*$github.getCollaborators(),*/ $github.getContributors(), $github.getIssues()])
+  		.then(function (results) {
+  		
+	  		var commits = results[0],
+//	  			collaborators = results[1],
+	  			contributors = results[1],
+	  			issues = results[2];
+
+	  		angular.extend($scope, {
+	  			github: {
+	  				commits: {
+	  					latest: commits.length ? commits[0] : {},
+	  					all: commits
+	  				},
+	  				issuesCount: issues.length,
+	  				issues: issues,
+//	  				collaborators: collaborators,
+	  				contributors: contributors,
+	  				actors: (function () {
+	  					
+	  				}())
+	  			}
+	  		});
+	  		
+			$log.info('GitHub data', $scope.github);
+			
+	  	}, function (err) {
+	  		$log.error(err);
+	  		$scope.github = null;
+	  	});  
+  });
