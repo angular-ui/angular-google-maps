d357bb81f4f260092cb83a4ee4e3416271f9bc6f
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-10 17:33:25 -0500

    Headlines moved to headlines controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,4 @@ angular.module('angularGoogleMapsApp')
     	zoom: 8
     };
 
-    $http({
-    	method: 'GET',
-    	url: '/headlines.json'
-    }).then(function (res) {
-    	$scope.headlines = res.data.items;
-    }, function (res) {
-    	$log.error('could not fetch headlines', res.status);
-    });
   });
