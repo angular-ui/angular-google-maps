9070c9483130a8d7df8dc519d9b4cb20d111964d
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-14 08:00:23 -0500

    Dont rev font files

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,8 +164,8 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
+            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'/*,
+            '<%= yeoman.dist %>/styles/fonts/*'*/
           ]
         }
       }
