e1642e10a604c9bcc4f250bf6a721deb623df92e
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-15 11:30:17 -0500

    Remove Chrome and IE, seems to cause problems on travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS', 'Chrome', 'IE'],
+    browsers: ['PhantomJS'/*, 'Chrome', 'IE'*/],
 
 
     // Continuous Integration mode
