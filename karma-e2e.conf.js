95a35739011584dd91e1cee836641d93c04dda16
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-15 11:18:05 -0500

    Include Chrome, PhantomJS and IE

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -37,12 +37,12 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'PhantomJS', 'IE'],
 
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
