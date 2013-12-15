bcc2681de77966ad47e587c6cd1927028be0c042
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-15 11:18:14 -0500

    Remove $scope.awesomeThings

--- app/scripts/controllers/use.js
+++ app/scripts/controllers/use.js
@@ -2,9 +2,4 @@
 
 angular.module('angularGoogleMapsApp')
   .controller('UseCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
