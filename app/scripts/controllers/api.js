0eb2df1a72eec9027289a656588e5ddc076a491e
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-24 10:18:58 -0500

    Implement directive search by name

--- app/scripts/controllers/api.js
+++ app/scripts/controllers/api.js
@@ -2,17 +2,20 @@
 
 angular.module('angularGoogleMapsApp').controller('ApiCtrl', function ($scope, $anchorScroll, $timeout, $location) {
 
-    $scope.directives = [
-    	'google-map',
-    	'marker',
-    	'markers',
-    	'label',
-    	'polygon',
-    	'polyline',
-    	'trafficlayer',
-    	'window',
-    	'windows'
+    // TODO make this auto-generated from jsdoc
+    var documentedDirectives = [
+        'google-map',
+        'marker',
+        'markers',
+        'label',
+        'polygon',
+        'polyline',
+        'trafficlayer',
+        'window',
+        'windows'
     ];
+
+    $scope.displayedDirectives = documentedDirectives;
 
     $scope.viewUrl = function (directive) {
     	return 'views/directive/' + directive + '.html';
@@ -21,7 +24,16 @@ angular.module('angularGoogleMapsApp').controller('ApiCtrl', function ($scope, $
     $scope.query = null;
     
     $scope.$watch('query', function (nVal, oVal) {
-    	
+        if (nVal !== oVal) {
+            if (nVal && nVal !== "") {
+                $scope.displayedDirectives = _.filter(documentedDirectives, function (value) {
+                    return value.indexOf(nVal) !== -1;
+                });
+            }
+            else {
+                $scope.displayedDirectives = documentedDirectives;
+            }
+        }
     });
 
 	$anchorScroll();    
