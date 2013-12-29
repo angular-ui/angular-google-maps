a4a51ebade1f7b94faaffdac71c154e27c765bd0
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-29 14:12:46 -0500

    Revamp Github info and add activity

--- app/scripts/controllers/footer.js
+++ app/scripts/controllers/footer.js
@@ -3,12 +3,13 @@
 angular.module('angularGoogleMapsApp').controller('FooterCtrl', function ($scope, $log, $q, $github) {
 
     // GitHub api calls
-  	$q.all([$github.getCommits(), $github.getContributors(), $github.getIssues()])
+  	$q.all([$github.getCommits(), $github.getContributors(), $github.getIssues(), $github.getEvents()])
   		.then(function (results) {
   		
 	  		var commits = results[0],
 	  			contributors = results[1],
-	  			issues = results[2];
+	  			issues = results[2],
+	  			events = results[3];
 
 	  		angular.extend($scope, {
 	  			github: {
@@ -19,16 +20,50 @@ angular.module('angularGoogleMapsApp').controller('FooterCtrl', function ($scope
 	  				issuesCount: issues.length,
 	  				issues: issues,
 	  				contributors: contributors,
-	  				actors: (function () {
-	  					
-	  				}())
+	  				events: events
 	  			}
 	  		});
-	  		
-			$log.info('GitHub data', $scope.github);
 			
 	  	}, function (err) {
 	  		$log.error(err);
 	  		$scope.github = null;
 	  	});  
+	  	
+	function actorLink (actor) {
+		return '<a href="' + actor.url + '" rel="external">' + actor.login + '</a>';
+	}
+	
+	$scope.eventLabel = function (event) {
+	
+		var pl = event.payload;
+		
+		switch (event.type) {
+		case 'WatchEvent':
+			return 'starred this repository';
+			
+		case 'CreateEvent':
+			return 'created ' + pl.ref_type + ' ' + pl.ref;
+			
+		case 'ForkEvent':
+			return 'forked this repository';
+			
+		case 'PushEvent':
+			return 'pushed ' + pl.size + ' commit(s) to ' + pl.ref.replace('refs/heads/', '');
+			
+		case 'IssueCommentEvent':
+			return 'commented on issue ' + pl.issue.number;
+			
+		case 'DeleteEvent':
+			return 'deleted ' + pl.ref_type + ' ' + pl.ref;
+			
+		case 'PullRequestEvent':
+			return pl.action + ' pull request ' + pl.pull_request.number;
+			
+		case 'IssuesEvent':
+			return pl.action + ' issue ' + pl.issue.number;
+			
+		}
+		
+		return "TODO (" + event.type + ")";
+	};
   });
