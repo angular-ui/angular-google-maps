3e8bfb40447eafd9c16c30ae0e25c1b0f5177d31
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-29 14:19:52 -0500

    Fix tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -15,14 +15,4 @@ describe('Controller: MainCtrl', function () {
       $scope: scope
     });
   }));
-
-  it('should attach a map object to the scope', function () {
-    expect(scope.map).not.toBe(null);
-  });
-
-  it('should provide minimal properties for the map to work', function () {
-  	expect(scope.map.center.latitude).not.toBe(null);
-  	expect(scope.map.center.longitude).not.toBe(null);
-  	expect(scope.map.zoom).not.toBe(null);
-  });
 });
