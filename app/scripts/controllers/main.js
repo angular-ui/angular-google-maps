b954d43ebf6cb6b8cbfa3e93165549a2c71f5f31
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-29 14:12:18 -0500

    Remove map from scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,6 @@
 'use strict';
 
 angular.module('angularGoogleMapsApp')
-  .controller('MainCtrl', function ($scope, $http, $log) {
-    
-    $scope.map = {
-    	center: {
-    		latitude: 45,
-    		longitude: -73
-    	},
-    	zoom: 8
-    };
+  .controller('MainCtrl', function ($scope) {
 
   });
