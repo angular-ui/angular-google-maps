4af0fb0b9bb1b73f62fb1610f6419e46c2185a84
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-12-29 19:48:13 -0500

    Add map back

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,19 @@
 angular.module('angularGoogleMapsApp')
   .controller('MainCtrl', function ($scope) {
 
+	$scope.map = {
+		center: {
+			latitude: 40.47,	// NYC
+			longitude: -73.85	// NYC
+		},
+		zoom: 8
+	};
+	
+	$scope.marker = {
+		coords: {
+			latitude: 40.47,
+			longitude: -74.50
+		}
+	};
+	
   });
