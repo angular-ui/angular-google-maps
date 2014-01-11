1a628a6bb70203edb7ad5c5ef55e6a63a9622f7e
Nick McCready <nmccready@gmail.com>
2014-01-11 16:15:01 -0500

    version for rawgithub test

--- example/example-controller.js
+++ example/example-controller.js
@@ -11,7 +11,7 @@ function ExampleController($scope, $timeout, $log, $http) {
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
 
-    $http.get("/package.json").success(function (data) {
+    $http.get("../package.json").success(function (data) {
         if (!data)
             console.error("no version object found!!");
         $scope.version = data.version;
