21d1730d9e61521eb62b40659b47f0c29ecfc557
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-01-12 17:04:33 -0500

    Add target _blank

--- app/scripts/directives/google-api-ref.js
+++ app/scripts/directives/google-api-ref.js
@@ -6,11 +6,14 @@ angular.module('angularGoogleMapsApp')
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
       
+      	$log.debug('generating link to Google Maps API reference for ' + attrs.googleApi);
+      
       	// Open links wil rel='external' in new window/tab      	
       	var el = angular.element(element);
       	
       	el.attr('href', 'https://developers.google.com/maps/documentation/javascript/reference#' + attrs.googleApi)
-      		.attr('rel', 'external');
+      		.attr('rel', 'external')
+      		.attr('target', '_blank');
       }
     };
   });
