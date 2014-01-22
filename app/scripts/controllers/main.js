c5c9b8f72696b2d24af7fcdfcf175fba426fcb87
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-01-22 07:17:06 -0500

    Get latest version from GitHub api call

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('angularGoogleMapsApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $github, $log) {
 
+	var DOWNLOAD_URL_TEMPLATE = 'https://raw.github.com/nlaplante/angular-google-maps/%REF%/dist/angular-google-maps.min.js',
+		FALLBACK_BRANCH = 'master';
+	
 	$scope.map = {
 		center: {
 			latitude: 40.47,	// NYC
@@ -18,4 +21,14 @@ angular.module('angularGoogleMapsApp')
 		}
 	};
 	
+	$github.getTags().then(function (data) {
+		$scope.latestTag = data && data.length ? data[0] : {};
+		$scope.downloadUrl = DOWNLOAD_URL_TEMPLATE.replace('%REF%', $scope.latestTag.name);
+	}, function (e) {
+	
+		$log.error('could not fetch latest tag; falling back to ' + FALLBACK_BRANCH, e);
+		
+		$scope.latestTag = { name: FALLBACK_BRANCH };
+		$scope.downloadUrl = DOWNLOAD_URL_TEMPLATE.replace('%REF%', FALLBACK_BRANCH);
+	});
   });
