1cda301534e0eaa12c9b67fd7f6b48b20dc6af3e
Nicolas Laplante <nlaplante@users.noreply.github.com>
2014-01-27 07:25:07 -0500

    Use $window.ga everytime

--- app/scripts/services/ga.js
+++ app/scripts/services/ga.js
@@ -26,18 +26,19 @@ angular.module('angularGoogleMapsApp').provider('analytics', function () {
 
 	this.$get = function ($window, $log, $rootScope, $document, $location) {
 
-		ga = $window.ga || function () {};
-
-//		ga.push(['_setAccount', _trackingCode]);
-
-
 		var _trackPageView = function (path) {
 				$log.debug('analytics: tracking page view', path);
-				ga('send', 'pageView', path);
+				
+				if ($window.ga) {
+					$window.ga('send', 'pageView', path);
+				}
 			},
 			_trackEvent = function (name, value) {
 				$log.debug('analytics: tracking event', { 'name': name, 'value': value });
-				ga('send', 'event', 'button', 'click', 'download library')
+				
+				if ($window.ga) {
+					$window.ga('send', 'event', 'button', 'click', 'download library')
+				}
 			};
 
 		if (_trackViewChange) {
