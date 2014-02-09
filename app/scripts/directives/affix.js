f7071cdaa90a85e0841e612ebb73400b5e831a30
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-02-09 15:13:30 -0500

    Directive for affix

---
+++ app/scripts/directives/affix.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('angularGoogleMapsApp')
+  .directive('affix', function ($log) {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {      
+
+      	var opts = {
+      		offset: {}
+      	};
+      	
+      	if (attrs.offsetTop) {
+      		opts.offset.top = parseInt(attrs.offsetTop);
+      	}
+      	
+      	if (attrs.offsetBottom) {
+      		opts.offset.bottom = parseInt(attrs.offsetBottom);
+      	}
+      	
+      	$log.debug('affix options', opts);
+      	
+      	angular.element(element).affix(opts);
+      }
+    };
+  });
