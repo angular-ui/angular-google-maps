2c8e563ce99f2a10977e7f2fc3354293223a6d72
Nick McCready <nmccready@gmail.com>
2014-02-17 19:26:57 -0500

    version bump 1.0.13

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 1.0.12 2014-02-17
+/*! angular-google-maps 1.0.13 2014-02-17
  *  AngularJS directives for Google Maps
  *  git: https://github.com/nlaplante/angular-google-maps.git
  */
