1b9149d691e877d83a0465515630ec563c8e89e5
nick.mccready <nmccready@gmail.com>
2014-04-17 19:06:01 -0400

    compiled changes from previous merge

--- dist/angular-google-maps.min.js
+++ dist/angular-google-maps.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 1.1.0-SNAPSHOT 2014-04-16
+/*! angular-google-maps 1.1.0-SNAPSHOT 2014-04-17
  *  AngularJS directives for Google Maps
  *  git: https://github.com/nlaplante/angular-google-maps.git
  */
