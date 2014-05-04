a8f181e49f4de64362e117f1d283b9b22651d77e
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-05-04 15:01:07 -0400

    Add changelog controller

---
+++ app/scripts/controllers/changelog.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('angularGoogleMapsApp')
+  .controller('ChangeLogCtrl', function ($scope, $log, changelog) {
+  
+  	// We need to manipulate the raw changelog json a bit to order by tag number and group commits
+  	// by author.
+  	
+  	var cl = [];
+  	
+  	for (var tag in changelog) {
+  		var commits = changelog[tag];
+  		
+  		cl.push({
+  			tag: tag,
+  			commits: _.groupBy(commits, function (value) {
+  				return value.author;
+	  		})
+  		});
+  	}
+  	
+  	// Sort by tag number in reverse order
+  	cl = _.sortBy(cl, function (value, key) {
+  		var tag = value.tag,
+  			replaced = value.tag.replace(/\./g, '');  		
+  			
+  		return parseInt(replaced);
+  	}).reverse();
+  	
+  	// Everything's set!
+  	$scope.changelog = cl;
+  });
