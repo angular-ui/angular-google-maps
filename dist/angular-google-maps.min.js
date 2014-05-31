1346b6a7b417aaca88db0519eb390314c64d0313
nick.mccready <nmccready@gmail.com>
2014-05-31 10:16:19 -0400

    1.1.0 in dist header

--- dist/angular-google-maps.min.js
+++ dist/angular-google-maps.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 1.1.1-SNAPSHOT 2014-05-26
+/*! angular-google-maps 1.1.0 2014-05-31
  *  AngularJS directives for Google Maps
  *  git: https://github.com/nlaplante/angular-google-maps.git
  */
