edc454829e2105b2f78bcae8c7d7089644be803f
nick.mccready <nmccready@gmail.com>
2014-06-28 15:01:37 -0400

    verison bump and skip 1.1.5 , due to bad tag

--- dist/angular-google-maps.min.js
+++ dist/angular-google-maps.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 1.1.4 2014-06-28
+/*! angular-google-maps 1.1.6 2014-06-28
  *  AngularJS directives for Google Maps
  *  git: https://github.com/nlaplante/angular-google-maps.git
  */
