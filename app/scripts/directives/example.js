824d98cb4701be088ae27190e62488f778bf0d43
Myles Bostwick <mbostwick@in-situ.com>
2014-07-30 00:40:51 -0600

    Fix path issues for when publishing to gh-pages.

--- app/scripts/directives/example.js
+++ app/scripts/directives/example.js
@@ -4,7 +4,22 @@ angular.module('angularGoogleMapsApp')
     .directive('runnableExample', function (openPlnkr) {
         return {
             restrict: 'E',
-            templateUrl: '/views/examples/base/template.html',
+            template: '<div>' +
+                '<div>' +
+                    '<button ng-click="click()">index.html</button>' +
+                    '<button ng-click="click()">script.js</button>' +
+                    '<button style="float:right" ng-click="editPlnkr()">Edit in Plnkr</button>' +
+                '</div>' +
+                '<div ng-show="index">' +
+                '<pre ng-non-bindable></pre>' +
+                '</div>' +
+        '<div ng-show="script">' +
+            '<pre ng-non-bindable></pre>' +
+        '</div>' +
+        '<div style="width:100%; height: 350px; padding:4px;">' +
+            '<iframe style="width: 100%; height: 100%"></iframe>' +
+            '</div>' +
+        '</div>',
             scope: {
                 example: "=example"
             },
@@ -12,6 +27,7 @@ angular.module('angularGoogleMapsApp')
                 console.log("Example controller.");
                 $scope.index = true;
                 $scope.script = false;
+                var prefix = window.location.pathname + "views/examples/";
 
                 $scope.click = function() {
                     $scope.index = !$scope.index;
@@ -19,21 +35,21 @@ angular.module('angularGoogleMapsApp')
                 };
 
                 $scope.editPlnkr = function() {
-                    openPlnkr("/views/examples/" + $scope.example);
+                    openPlnkr(prefix + $scope.example);
                 };
 
                 var pres = $element.find('pre');
-                $http.get("/views/examples/" + $scope.example + "/index.html").then(function(index) {
+                $http.get(prefix + $scope.example + "/index.html").then(function(index) {
                     console.log("fetched index", index);
                     pres[0].innerText = index.data;
                 });
-                $http.get("/views/examples/" + $scope.example + "/script.js").then(function(script) {
+                $http.get(prefix + $scope.example + "/script.js").then(function(script) {
                     console.log("fetched script", script);
                     pres[1].innerText = script.data;
                 });
 
                 var iframe = $element.find("iframe");
-                iframe.attr("src", "/views/examples/base/base.html?example=" + $scope.example);
+                iframe.attr("src", prefix + "base/base.html?example=" + $scope.example);
             }]
         };
     });
