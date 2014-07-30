5422fd3586a396fdadda02943eb50af0eb1b90fb
Myles Bostwick <mbostwick@in-situ.com>
2014-07-30 16:38:43 -0600

    Update map-control example so template is loaded and update version of nggmap

--- app/views/examples/map-control/script.js
+++ app/views/examples/map-control/script.js
@@ -1,7 +1,4 @@
 angular.module('appMaps', ['google-maps'])
-    .run(function ($templateCache) {
-        $templateCache.put('control.tpl.html', '<button class="btn btn-sm btn-primary" ng-class="{\'btn-warning\': danger}" ng-click="controlClick()">{{controlText}}</button>');
-    })
     .controller('mainCtrl', function ($scope, $log) {
         $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
         $scope.options = {scrollwheel: false};
