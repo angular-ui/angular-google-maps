dac5aacf37914aa4e8a09d58c84f7211c5fba1a1
Nick McCready <nmccready@gmail.com>
2014-08-01 14:56:27 -0400

    examples fixed pointing website to 1.2.0, download button now filters SNAPSHOT

--- app/views/examples/marker-label/script.js
+++ app/views/examples/marker-label/script.js
@@ -3,6 +3,7 @@ angular.module('appMaps', ['google-maps'])
         $scope.map = {center: {latitude: 40, longitude: -99 }, zoom: 12 }
         $scope.options = {scrollwheel: false};
         $scope.marker = {
+            id:0,
             title: "Fancy Title!",
             coords: {
                 latitude: 40,
