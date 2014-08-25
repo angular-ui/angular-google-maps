e855db85a31aef30c4c28266c33ec2cf1a2596c2
Nick McCready <nmccready@gmail.com>
2014-08-25 13:52:34 -0400

    async GoogeMapAPI loading on #654 example

--- example/assets/scripts/controllers/issue-654-marker-events-break-after-added.js
+++ example/assets/scripts/controllers/issue-654-marker-events-break-after-added.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', ['google-maps'.ns()]);
-app.controller('TrackingCtrl', ['$rootScope', '$scope', '$timeout', '$log',
-  function ($rootScope, $scope, $timeout, $log) {
+app.controller('TrackingCtrl', ['$rootScope', '$scope', '$timeout', '$log', 'GoogleMapApi'.ns(),
+  function ($rootScope, $scope, $timeout, $log, GoogleMapApi) {
     $scope.map = {
       center: {
         latitude: 53.406754,
@@ -10,8 +10,11 @@ app.controller('TrackingCtrl', ['$rootScope', '$scope', '$timeout', '$log',
       zoom: 5,
       refresh: false,
       events: {},
-      bounds: {},
-      markers: [
+      bounds: {}
+    };
+    //when the API is really ready and loaded play w/ the scope
+    GoogleMapApi.then(function (map) {
+      $scope.map.markers = [
         {
           id: 1,
           location: {
@@ -23,30 +26,40 @@ app.controller('TrackingCtrl', ['$rootScope', '$scope', '$timeout', '$log',
           },
           showWindow: false
         }
-      ],
-      markerEvents: {
+      ];
+      $scope.map.markerEvents = {
         dblclick: function (gMarker, eventName, model, latLngArgs) {
           var id = model.idKey || model.id;
           alert("Marker double clicked! Model: " + id);
         }
       }
-    };
-    $timeout(function () {
-      $scope.map.markers[0].latitude = 53.416754;
-      $scope.map.markers[0].longitude = -2.148843;
+      $timeout(function () {
+        $scope.map.markers[0].latitude = 53.416754;
+        $scope.map.markers[0].longitude = -2.148843;
 
-      _.range(10).forEach(function (i) {
-        $scope.map.markers.push({
-          id: i + 1,
-          location: {
-            latitude: $scope.map.markers[0].latitude + 1*i,
-            longitude: $scope.map.markers[0].longitude + 1*i
-          },
-          options: {
-            title: 'Marker2'
-          },
-          showWindow: false
-        })
-      });
-    }, 1000);
-  }]);
+        _.range(10).forEach(function (i) {
+          $scope.map.markers.push({
+            id: i + 1,
+            location: {
+              latitude: $scope.map.markers[0].latitude + 1 * i,
+              longitude: $scope.map.markers[0].longitude + 1 * i
+            },
+            options: {
+              title: 'Marker2'
+            },
+            showWindow: false
+          })
+        });
+      }, 1000);
+    });
+}])
+.
+config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
+  GoogleMapApi.configure({
+//    key: 'your api key',
+    v: '3.16',
+    libraries: 'weather,geometry,visualization'
+  });
+}])
+
+;
