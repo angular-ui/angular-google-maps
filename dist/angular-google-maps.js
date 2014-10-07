d4ee96ab4597c89ee60d0824ee7625276895dc3e
Nick McCready <nmccready@gmail.com>
2014-10-06 21:06:12 -0400

    2.0.0-SNAPSHOT-d to indicate that this is a stable version almost ready for release

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.0.0-SNAPSHOT 2014-10-06
+/*! angular-google-maps 2.0.0-SNAPSHOT-d 2014-10-06
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
@@ -6659,7 +6659,7 @@ angular.module('google-maps.wrapped'.ns()).service('GoogleMapsUtilV3'.ns(), func
   return {
     init: _.once(function () {
       //BEGIN REPLACE
-      /*! angular-google-maps 2.0.0-SNAPSHOT 2014-10-06
+      /*! angular-google-maps 2.0.0-SNAPSHOT-d 2014-10-06
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
