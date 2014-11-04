7e00a8791d68dd57d2c6782b3402b2b8936b4bfa
Aaron Ellis Snyder <snyder@mojotech.com>
2014-11-04 17:31:19 -0500

    don't redraw on idle

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -8381,6 +8381,7 @@ MarkerClusterer.prototype.onAdd = function () {
             }
         }),
         google.maps.event.addListener(this.getMap(), "idle", function () {
+            console.log('yay yay');
             cMarkerClusterer.redraw_();
         })
     ];
@@ -9277,7 +9278,6 @@ if (typeof String.prototype.trim !== 'function') {
         return this.replace(/^\s+|\s+$/g, '');
     }
 }
-
 ;/**
  * 1.1.9-patched
  * @name MarkerWithLabel for V3
@@ -10192,6 +10192,30 @@ angular.module('google-maps.extensions'.ns()).service('ExtendMarkerClusterer'.ns
               }
               return this;
             }).apply(obj1, [obj2]);
+          };
+
+          NgMapMarkerClusterer.prototype.onAdd = function() {
+            var cMarkerClusterer = this;
+
+            this.activeMap_ = this.getMap();
+            this.ready_ = true;
+
+            this.repaint();
+
+            // Add the map event listeners
+            this.listeners_ = [
+                google.maps.event.addListener(this.getMap(), "zoom_changed", function () {
+                    cMarkerClusterer.resetViewport_(false);
+                    // Workaround for this Google bug: when map is at level 0 and "-" of
+                    // zoom slider is clicked, a "zoom_changed" event is fired even though
+                    // the map doesn't zoom out any further. In this situation, no "idle"
+                    // event is triggered so the cluster markers that have been removed
+                    // do not get redrawn. Same goes for a zoom in at maxZoom.
+                    if (this.getZoom() === (this.get("minZoom") || 0) || this.getZoom() === this.get("maxZoom")) {
+                        google.maps.event.trigger(this, "idle");
+                    }
+                })
+            ];
           };
 
           return NgMapMarkerClusterer;
