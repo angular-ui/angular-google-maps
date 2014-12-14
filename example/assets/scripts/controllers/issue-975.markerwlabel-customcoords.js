aa57ef122a66475346c58e2ee6c5669202fa2504
Nick McCready <nmccready@gmail.com>
2014-12-14 13:33:58 -0500

    example actually working like what was on the plunkr

--- example/assets/scripts/controllers/issue-975.markerwlabel-customcoords.js
+++ example/assets/scripts/controllers/issue-975.markerwlabel-customcoords.js
@@ -1,17 +1,19 @@
-angular.module('app', ['uiGmapgoogle-maps'])
+angular.module("markerCoordsTest", ['uiGmapgoogle-maps'])
+
   .config(['uiGmapGoogleMapApiProvider', function (GoogleMapApi) {
     GoogleMapApi.configure({
       v: '3.17',
       libraries: ''
     });
   }])
-  .controller("ctrl", function ($scope, $interval, uiGmapLogger) {
-    uiGmapLogger.currentLevel = uiGmapLogger.LEVELS.debug;
-    var icon = undefined;
+
+  .controller("TestController", function($scope, $interval) {
+
+    icon = undefined;
 
     $scope.map = {
       title: "A test map",
-      center: {latitude: 45.0, longitude: 11.0},
+      center: {latitude: 45.0,longitude: 11.0},
       zoom: 15
     };
 
@@ -24,7 +26,7 @@ angular.module('app', ['uiGmapgoogle-maps'])
           latitude: 45.0,
           longitude: 11.0
         },
-        options: {
+        options:{
           labelContent: "markerLabel with default 'coords'",
           labelAnchor: "5 0",
           labelClass: "marker-labels"
@@ -41,7 +43,7 @@ angular.module('app', ['uiGmapgoogle-maps'])
           latitude: 45.005,
           longitude: 11.005
         },
-        options: {
+        options:{
           labelContent: "markerLabel with custom 'coords'",
           labelAnchor: "5 0",
           labelClass: "marker-labels"
@@ -61,13 +63,15 @@ angular.module('app', ['uiGmapgoogle-maps'])
       }
     ];
 
-    $interval(function () {
+    $interval(function() {
       $scope.markersLabelOK[0].coords.latitude = $scope.markersLabelOK[0].coords.latitude + 0.0001;
       $scope.markersLabelOK[0].coords.longitude = $scope.markersLabelOK[0].coords.longitude + 0.0001;
-
+    }, 1500);
+    $interval(function() {
       $scope.markersLabelNotOK[0].xcoords.latitude = $scope.markersLabelNotOK[0].xcoords.latitude + 0.0001;
       $scope.markersLabelNotOK[0].xcoords.longitude = $scope.markersLabelNotOK[0].xcoords.longitude + 0.0001;
-
+    }, 1500);
+    $interval(function() {
       $scope.markersOK[0].xcoords.latitude = $scope.markersOK[0].xcoords.latitude + 0.0001;
       $scope.markersOK[0].xcoords.longitude = $scope.markersOK[0].xcoords.longitude + 0.0001;
     }, 1500);
