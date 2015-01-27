a3bad6fb5c7395ddef9d4e755064a24ee16169a9
Carrie Kengle <carriesteady@gmail.com>
2015-01-27 16:31:04 -0500

    build

--- dist/angular-google-maps_dev_mapped.js
+++ dist/angular-google-maps_dev_mapped.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.0.12 2015-01-21
+/*! angular-google-maps 2.0.12 2015-01-27
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
