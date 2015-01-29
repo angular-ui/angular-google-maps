8203bf2e4a21394ffa7465aa10913c6e9b5cb8d4
Nick McCready <nmccready@gmail.com>
2015-01-29 10:44:39 -0500

    build all

--- dist/angular-google-maps.min.js
+++ dist/angular-google-maps.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.0.12 2015-01-27
+/*! angular-google-maps 2.0.12 2015-01-29
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
