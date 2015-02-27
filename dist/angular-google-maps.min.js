530be050eb8c1b19651c61dc35605cf709d306bf
Nick McCready <nmccready@gmail.com>
2015-02-27 15:35:23 -0500

    Release 2.1.0-X.0

--- dist/angular-google-maps.min.js
+++ dist/angular-google-maps.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.1.0-X 2015-02-27
+/*! angular-google-maps 2.1.0-X.0 2015-02-27
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
