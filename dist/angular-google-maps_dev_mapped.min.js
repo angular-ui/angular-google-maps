42d45ce317be77491e2493f85d1ef9703b42f999
Nick McCready <nmccready@gmail.com>
2015-03-10 18:11:54 -0400

    Release 2.0.16

--- dist/angular-google-maps_dev_mapped.min.js
+++ dist/angular-google-maps_dev_mapped.min.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.0.15 2015-03-10
+/*! angular-google-maps 2.0.16 2015-03-10
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
