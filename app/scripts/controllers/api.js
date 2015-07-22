b0f6b8c674c4770960ee6ac09e1ec402420fae86
Paul Bulai <paul.cbulai@gmail.com>
2015-07-22 09:15:14 +0300

    added links to specific api documentation parts

--- app/scripts/controllers/api.js
+++ app/scripts/controllers/api.js
@@ -30,51 +30,59 @@ angular.module('angularGoogleMapsApp')
     'PropMap',
     'Promise'
   ])
-  .config([ '$stateProvider', 'directiveList', 'providerList', 'serviceList',
-    function ($stateProvider, directiveList, providerList, serviceList) {
-    [
-      {modules: directiveList, loc: 'directive/'},
-      {modules: providerList, loc: 'provider/'},
-      {modules: serviceList, loc: 'service/'}
-    ].forEach(function (modsToLoc) {
-        modsToLoc.modules.forEach(function (cur) {
-          (function (cur) {
+  .config(['$stateProvider', 'directiveList', 'providerList', 'serviceList',
+    function($stateProvider, directiveList, providerList, serviceList) {
+      [{
+        modules: directiveList,
+        loc: 'directive/'
+      }, {
+        modules: providerList,
+        loc: 'provider/'
+      }, {
+        modules: serviceList,
+        loc: 'service/'
+      }].forEach(function(modsToLoc) {
+        modsToLoc.modules.forEach(function(cur) {
+          (function(cur) {
             $stateProvider.state('api.' + cur, {
+              url: '/' + cur,
               templateUrl: 'views/' + modsToLoc.loc + cur + '.html'
             })
           })(cur)
         })
       });
-  }])
-  .controller('ApiCtrl',[ '$scope', '$rootScope', '$location', '$state',
-    'directiveList', 'providerList', 'serviceList','$anchorScroll',
-    function ($scope, $rootScope, $location, $state,
-      directiveList, providerList, serviceList, $anchorScroll) {
-    if ($state.current.name === "api") {
-      $state.go("api." + providerList[0]);
     }
-    $scope.providers = providerList;
-    $scope.services = serviceList;
-    $scope.directives = directiveList;
-    $scope.current = providerList[0];
-    $scope.current = $state.$current.name;
-
-    $rootScope.$on("$stateChangeSuccess", function (event, to) {
-      $scope.current = $state.$current.name.substring(4);
-    });
-
-    $scope.scrollTo = function(id) {
-       $location.hash(id);
-       $anchorScroll();
-     };
-
-    $scope.query = '';
-
-    $scope.$watch(function () {
-      return $location.hash();
-    }, function (newValue, oldValue) {
-      if (newValue !== oldValue) {
-        $('#content' + newValue).collapse('show');
+  ])
+  .controller('ApiCtrl', ['$scope', '$rootScope', '$location', '$state',
+    'directiveList', 'providerList', 'serviceList', '$anchorScroll',
+    function($scope, $rootScope, $location, $state,
+      directiveList, providerList, serviceList, $anchorScroll) {
+      if ($state.current.name === "api") {
+        $state.go("api." + providerList[0]);
       }
-    });
-  }]);
+      $scope.providers = providerList;
+      $scope.services = serviceList;
+      $scope.directives = directiveList;
+      $scope.current = providerList[0];
+      $scope.current = $state.$current.name;
+
+      $rootScope.$on("$stateChangeSuccess", function(event, to) {
+        $scope.current = $state.$current.name.substring(4);
+      });
+
+      $scope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll();
+      };
+
+      $scope.query = '';
+
+      $scope.$watch(function() {
+        return $location.hash();
+      }, function(newValue, oldValue) {
+        if (newValue !== oldValue) {
+          $('#content' + newValue).collapse('show');
+        }
+      });
+    }
+  ]);
