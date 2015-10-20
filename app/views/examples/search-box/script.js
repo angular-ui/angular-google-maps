fa299b82d53827fed7c6fb7176d8ee58c5df68e2
Carrie Kengle <carriesteady@gmail.com>
2015-10-20 08:29:59 -0400

    modified searchbox example

--- app/views/examples/search-box/script.js
+++ app/views/examples/search-box/script.js
@@ -1,16 +1,39 @@
 angular.module('appMaps', ['uiGmapgoogle-maps'])
-    .config(function(uiGmapGoogleMapApiProvider) {
-      uiGmapGoogleMapApiProvider.configure({
-          //    key: 'your api key',
-          v: '3.20', //defaults to latest 3.X anyhow
-          libraries: 'places' // Required for SearchBox.
-      });
-    })
-    .controller('mainCtrl', function ($scope, $log) {
-        $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-        $scope.options = {scrollwheel: false};
-        var events = {
-          places_changed: function (searchBox) {}
-        }
-        $scope.searchbox = { template:'searchbox.tpl.html', events:events};
+
+  .config(['uiGmapGoogleMapApiProvider', function (GoogleMapApi) {
+    GoogleMapApi.configure({
+  //  key: 'your api key',
+      v: '3.20',
+      libraries: 'places'
     });
+  }])
+  
+  .run(['$templateCache', function ($templateCache) {
+    $templateCache.put('searchbox.tpl.html', '<input type="text" placeholder="Search">');
+  }])
+
+  .controller('mainCtrl', ['$scope', '$log', 'uiGmapGoogleMapApi', function ($scope, $log, GoogleMapApi) {
+      
+      angular.extend($scope, {
+        map: {center: 
+          {
+            latitude: 40.1451, 
+            longitude: -99.6680  
+          }, 
+          zoom: 4 
+        },
+        searchbox: { 
+          template:'searchbox.tpl.html', 
+          events:{
+            places_changed: function (searchBox) {}
+          }
+        },
+        options: {
+          scrollwheel: false
+        }
+      });
+      
+      GoogleMapApi.then(function(maps) {
+        maps.visualRefresh = true;
+      });
+  }]);
